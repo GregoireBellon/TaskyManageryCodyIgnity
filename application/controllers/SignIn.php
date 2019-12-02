814ab88b0d478e17cd254345a992e3cb37bbf1cd
Colin Bouligand <i183225@IUTLAVALAD.UNIV-LEMANS.FR>
2019-12-02 15:33:56 +0100

    test afficher username

--- application/controllers/SignIn.php
+++ application/controllers/SignIn.php
@@ -3,9 +3,12 @@
 
 class SignIn extends CI_Controller
 {
-	public function verif()
+	public function verif($id_user)
 	{
-
+			$this->load->model('Users_model');
+			$user = $this->users_model->get_user($id_user);
+			$data['nom']=$user['nom_user'];
+			$this->load->view('movie_review',$data);
 	}
 
 }
--- application/models/Users_model.php
+++ application/models/Users_model.php
@@ -3,5 +3,23 @@
 
 class Users_model
 {
+	public function __construct()
+	{
+		$this->load->database();
+	}
+
+	public function get_user($id)
+	{
+		if($id !=FALSE)
+		{
+			$query = $this->db->get_where('Utilisateur', array('id'=> $id));
+			return $query->row_array();
+		}
+		else
+		{
+			return false;
+		}
+	}
+
 
 }
