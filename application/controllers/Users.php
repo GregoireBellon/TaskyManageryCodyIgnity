8db122fbe831c480b9e74c65944379b6ec739454
Colin Bouligand <i183225@IUTLAVALAD.UNIV-LEMANS.FR>
2019-12-02 16:18:14 +0100

    test affichage users

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -1,14 +1,14 @@
 <?php
 
 
-class SignIn extends CI_Controller
+class Users extends CI_Controller
 {
 	public function verif($id_user)
 	{
-			$this->load->model('Users_model');
+			$this->load->model('users_model');
 			$user = $this->users_model->get_user($id_user);
 			$data['nom']=$user['nom_user'];
-			$this->load->view('movie_review',$data);
+			$this->load->view('users_review',$data);
 	}
 
 }
